3de92875b5276420ea2a13389f687c12faafe1bb
Carmen Alvarez <calvarez@libon.com>
2019-01-15 13:41:58 +0100

    Add project config which works
    * use androidx
    * use jetifier
    * add an activity with a SwipeRefreshLayout
    * create a unit test which uses a Shadow of SwipeRefreshLayout
    * use gradle 4.10.2
    * use android gradle plugin 3.2.1

--- app/src/test/java/com/example/robolectric/jetifierissue/ExampleUnitTest.java
+++ app/src/test/java/com/example/robolectric/jetifierissue/ExampleUnitTest.java
@@ -1,17 +1,23 @@
 package com.example.robolectric.jetifierissue;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.support.v4.Shadows;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void exampleTest() {
+        MainActivity activity = Robolectric.buildActivity(MainActivity.class).create().resume().visible().get();
+        SwipeRefreshLayout swipeRefreshLayout = activity.findViewById(R.id.swipe_refresh_layout);
+        Shadows.shadowOf(swipeRefreshLayout);
     }
 }
